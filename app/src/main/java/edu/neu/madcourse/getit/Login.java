87a085367a738f34d5b0ba0538595b16cd749ffc
Akash Sasikumar <33842912+AkashSasikumar@users.noreply.github.com>
2021-04-03 15:25:57 -0400

    navigate back and forth between login and register

--- app/src/main/java/edu/neu/madcourse/getit/Login.java
+++ app/src/main/java/edu/neu/madcourse/getit/Login.java
@@ -5,25 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
-public class Register extends AppCompatActivity implements View.OnClickListener {
-
+public class Login extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
 
-        findViewById(R.id.loginHere).setOnClickListener(this);
+        findViewById(R.id.registerHere).setOnClickListener(this);
     }
-
 
     @Override
     public void onClick(View v) {
         Intent intent;
-        if (v.getId() == R.id.loginHere){
-            intent = new Intent(v.getContext(), Login.class);
+        if(v.getId() == R.id.registerHere){
+            intent = new Intent(v.getContext(), Register.class);
             v.getContext().startActivity(intent);
         }
     }
