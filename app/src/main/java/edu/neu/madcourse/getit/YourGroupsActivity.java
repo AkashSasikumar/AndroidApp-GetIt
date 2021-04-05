77acfed29b057be44b69b2a3fd2d5031a8deeaab
naveen497 <chnaveen497@gmail.com>
2021-04-04 22:19:42 -0400

    added your_groups layout

---
+++ app/src/main/java/edu/neu/madcourse/getit/YourGroupsActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.getit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YourGroupsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.your_groups);
+    }
+}
