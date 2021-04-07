14fe9f1b1cae7ccdf8133b4feb51d3499fb3bbab
Akash Sasikumar <33842912+AkashSasikumar@users.noreply.github.com>
2021-04-06 20:39:40 -0400

    Added YourGroupsActivity to manifest file and linked login page. Some errors in YourGroupsActivity adapter.

--- app/src/main/java/edu/neu/madcourse/getit/YourGroupsActivity.java
+++ app/src/main/java/edu/neu/madcourse/getit/YourGroupsActivity.java
@@ -25,12 +25,13 @@ public class YourGroupsActivity extends AppCompatActivity implements View.OnClic
         setContentView(R.layout.your_groups);
         mGroupName = findViewById(R.id.group_name_field);
         mGroupCode = findViewById(R.id.group_code_field);
+        join_group_btn = findViewById(R.id.join_group_btn);
         join_group_btn.setOnClickListener(this);
         final RecyclerView groupsRV = findViewById(R.id.recyclerView);
         final GroupsRVAdapter groupsRVAdapter = new GroupsRVAdapter(groups, this);
 
-        groupsRV.setAdapter(groupsRVAdapter);
-        groupsRV.setLayoutManager(new LinearLayoutManager(this));
+//        groupsRV.setAdapter(groupsRVAdapter);
+//        groupsRV.setLayoutManager(new LinearLayoutManager(this));
 
 
     }
@@ -39,7 +40,7 @@ public class YourGroupsActivity extends AppCompatActivity implements View.OnClic
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.join_group_btn) {
-            startActivity(new Intent(getApplicationContext(), Login.class));
+            //startActivity(new Intent(getApplicationContext(), Login.class));
             String groupCode = mGroupCode.getText().toString().trim();
             String groupName = mGroupCode.getText().toString().trim();
             if (TextUtils.isEmpty(groupName) && TextUtils.isEmpty(groupCode)) {
